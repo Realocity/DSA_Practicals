bef9ad9fe2e929e925ccc8889b264930c8b9aeab
shubham sapkal <66507418+Realocity@users.noreply.github.com>
2021-04-29 12:01:53 +0530

    Code updated

--- javascript/Divide and Conquer/Linear Search.js
+++ javascript/Divide and Conquer/Linear Search.js
@@ -1,17 +1,26 @@
-function searchAlgorithm (number){
- 
-    for(let i = 0; i< number; i++){
-    if(number == array[i]){
-      console.log("Found it, it's at index " + i)
-    }else{
-      console.log("Not found")
-    }
-    
+/*
+Name : Shubham Sapkal
+Roll No. : 2012118
+
+Output :
+Found: 38 in 7 steps
+Found: 42 in 9 steps
+*/
+const unsortedArr = [ 32, 49, 10, 12, 19, 24, 38, 5, 14, 44, 40, 3, 45, 39, 23, 2];
+
+const sortedArr = [16, 17, 18, 19, 20, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+
+
+
+const linear = (arr, target) => {
+  let steps = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    steps++;
+    if (arr[i] === target) return `Found: ${arr[i]} in ${steps} steps`;
   };
-  
-}
+};
 
-
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-searchAlgorithm(5)
+console.log(linear(unsortedArr, 38)); // 38 value in 7th Position
+console.log(linear(sortedArr, 42)); // 42 value in 9th Position
+ 
